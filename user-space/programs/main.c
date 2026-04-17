08711646565b95ebc38dca4125ca15fd406e8fcb
Arterio Rodrigues <arterio.rodrigues@jetblue.com>
2026-04-16 21:25:19 -0400

    remove malloc for now

--- user-space/programs/main.c
+++ user-space/programs/main.c
@@ -1,12 +1,6 @@
-#include "../lib/malloc.h"
 extern void sys_exec(char *filname, char *buf);
 
 void main() {
-    int *x = malloc(sizeof(int) * 10);
-  
-    for(int i = 0;  i < 10; i++)
-      x[i] = i;
-    
     sys_exec("shell", "");
     while (1)
         ;
